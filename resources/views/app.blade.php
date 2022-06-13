18f78edd6be9a4f4709fea45d71a669b18baf0b5
André Melo <andrelucassmelo@gmail.com>
2022-06-13 19:07:14 -0300

    Ajustado criação de usuario com padrão 'user'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,16 +41,16 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                    <x-responsive-nav-link href="route('logout')" class="btn btn-dark" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
                 @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                <a href="{{ route('login') }}" class="btn btn-dark">Log in</a>
 
                 @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                <a href="{{ route('register') }}" class="btn btn-dark">Register</a>
                 @endif
                 @endauth
             </div>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('titulo', 'Sessões')
+
+@section('conteudo')
+<img src="https://http.cat/404" alt="404gatin" style="display: block; margin-left: auto; margin-right: auto; width: 50%;">
+@endsection
