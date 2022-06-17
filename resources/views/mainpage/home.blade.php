11004e5a473ed5ec095362ae51b869fd7ae4d367
André Melo <andrelucassmelo@gmail.com>
2022-06-17 11:15:44 -0300

    Ajuste de pesquisa

--- resources/views/mainpage/home.blade.php
+++ resources/views/mainpage/home.blade.php
@@ -97,7 +97,7 @@
         $(document).ready(function() {
             $("#input-text").on("keyup", function() {
                 var value = $(this).val().toLowerCase();
-                $("#myTable tr").filter(function() {
+                $("#myTable tbody tr").filter(function() {
                     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
                 });
             });
